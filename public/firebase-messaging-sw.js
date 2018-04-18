a288e063eca43d5cb02b6a8596268ae0fe7f85d5
durgesh <durgesh.ahirwar@systematixindia.com>
2018-04-18 20:49:50 +0530

    worked on push notification

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -18,7 +18,3 @@ importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
 // messages.
-const messaging = firebase.messaging();
- messaging.onMessage(function(payload){
-   console.log(payload);
- })
